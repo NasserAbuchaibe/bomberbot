ccb572d867664e172f104375dcf9c1f4c637f9b8
Nasser Abuchaibe <60364142+NasserAbuchaibe@users.noreply.github.com>
2020-10-07 00:35:23 -0500

    Update models.py
    add new fields

--- fake_api/models.py
+++ fake_api/models.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 
 # Create your models here.
 
-class StudentModel(models.Model):
+class StudentModel(models.del):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     firts_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -12,16 +12,25 @@ class StudentModel(models.Model):
     password = models.CharField(max_length=100)
     parent = models.ForeignKey('ParentsModel', on_delete=models.CASCADE, default=None)
     teacher = models.CharField(max_length=100)
-    classroom_id = models.CharField(max_length=60, unique=True)
+    classroom_id = models.UUIDField(default=uuid4, editable=False)
     access_code = models.CharField(max_length=60)
     selected_course = models.CharField(max_length=60)
-    owner_project = models.CharField(max_length=100)
-    description_project = models.TextField()
-    reference_project = models.CharField(max_length=60)
+    owner_project1 = models.CharField(max_length=100, null= True, blank=True)
+    description_project1 = models.TextField(null= True, blank=True)
+    reference_project1 = models.CharField(max_length=60,  null= True, blank=True)
+    score_project1 = models.FloatField(null= True, blank=True)
+    project_goals1 = models.TextField(null= True, blank=True)
+    owner_project2 = models.CharField(max_length=100, null= True, blank=True)
+    description_project2 = models.TextField(null= True, blank=True)
+    reference_project2 = models.CharField(max_length=60, null= True, blank=True)
+    score_project2 = models.FloatField(null= True, blank=True)
+    project_goals2 = models.TextField(null= True, blank=True)
     cover_image1 = models.URLField(max_length=150)
     cover_image2 = models.URLField(max_length=150)
     last_time = models.DateTimeField(null=True, blank=True)
     total_time = models.FloatField(default=0)
+    auth_parent = models.BooleanField(null=True, blank=True)
+    
     class Meta:
         ordering = ['username']
 
